c1f4c74326d10ec926974178f206b650330131d5
madhuri1127 <68366270+madhuri1127@users.noreply.github.com>
2020-08-26 16:07:58 +0530

    Update opportunitystagechange.js

--- modules/opportunitystagechange.js
+++ modules/opportunitystagechange.js
@@ -2,45 +2,44 @@
 
 let auth = require("./slack-salesforce-auth"),
     force = require("./force"),
-    OPPSTAGE_TOKEN = process.env.SLACK_OPPSTAGE_TOKEN;
+    CHANGESTAGE_TOKEN = process.env.SLACK_CHANGESTAGE_TOKEN;
 
 exports.execute = (req, res) => {
 
-    if (req.body.token != OPPSTAGE_TOKEN) {
+    if (req.body.token != CHANGE_TOKEN) {
         res.send("Invalid token");
         return;
     }
 
     let slackUserId = req.body.user_id,
         oauthObj = auth.getOAuthObject(slackUserId),
-q = "SELECT Id, Name, Amount, Probability, StageName, CloseDate FROM Opportunity WHERE Name LIKE '%" + req.body.text + "%' LIMIT 5";
-       
-
-  force.query(oauthObj, q)
+        params = req.body.text.split(":"),
+        StageName = params[1],
+        name = params[0],
+        
+ q = "SELECT Id, Name, Amount, Probability, StageName, CloseDate FROM Opportunity WHERE Name LIKE '%" + name + "%' LIMIT 1";
+let re = force.query(q);
+let cooper = re.records;
+cooper.StageName =  StageName;
+     force.sobject('Opportunity').update(cooper);
         .then(data => {
             let opportunities = JSON.parse(data).records;
             if (opportunities && opportunities.length > 0) {
                 let attachments = [];
                 opportunities.forEach(function (opportunity) {
                     let fields = [];
-                    fields.push({title: "Opportunity", value: opportunity.Name, short: true});
-                    fields.push({title: "Stage", value: opportunity.StageName, short: true});
-                    fields.push({
-                        title: "Amount",
-                        value: new Intl.NumberFormat('en-US', {
-                            style: 'currency',
-                            currency: 'USD'
-                        }).format(opportunity.Amount),
-                        short: true
-                    });
-                    fields.push({title: "Probability", value: opportunity.Probability + "%", short: true});
+                    
+                    
                     fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + opportunity.Id, short:false});
                     attachments.push({
                         color: "#FCB95B",
                         fields: fields
                     });
                 });
-                 res.json({text: "Opportunities matching '" + req.body.text + "':", attachments: attachments});
+                res.json({
+                    text: "StageName Changed Successfully"
+                    attachments: attachments
+                });
             } else {
                 res.send("No records");
             }
