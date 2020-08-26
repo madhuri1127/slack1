140439d15ae6b2812feb735e89a3fcc364c5b5fe
madhuri1127 <68366270+madhuri1127@users.noreply.github.com>
2020-08-26 16:52:54 +0530

    Update opportunitystagechange.js

--- modules/opportunitystagechange.js
+++ modules/opportunitystagechange.js
@@ -21,32 +21,9 @@ exports.execute = (req, res) => {
 let re = force.query(q);
 let cooper = re.records;
 cooper.StageName =  StageName;
-     force.sobject('Opportunity').update(cooper)
-        .then(data => {
-            let opportunities = JSON.parse(data).records;
-            if (opportunities && opportunities.length > 0) {
-                let attachments = [];
-                opportunities.forEach(function (opportunity) {
-                    let fields = [];
-                    
-                    
-                    fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + opportunity.Id, short:false});
-                    
-                });
-                res.json({
-                    text: "StageName Changed Successfully"
-                    
-                });
-            } else {
-                res.send("No records");
-            }
-        })
-        .catch(error => {
-            if (error.code == 401) {
-                res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
-            } else {
-                console.log(error);
-                res.send("An error as occurred");
-            }
-        });
+ let ret=    force.sobject('Opportunity').update(cooper)
+        
+      if (ret.success) {
+    console.log('Stage updated in Salesforce.');
+} 
 };
