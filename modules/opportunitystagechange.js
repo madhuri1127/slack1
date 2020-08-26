662659da1fa9f9f3c6ad8803d62df8e6db7a3cd1
madhuri1127 <68366270+madhuri1127@users.noreply.github.com>
2020-08-26 18:14:33 +0530

    Update opportunitystagechange.js

--- modules/opportunitystagechange.js
+++ modules/opportunitystagechange.js
@@ -15,35 +15,23 @@ exports.execute = (req, res) => {
         oauthObj = auth.getOAuthObject(slackUserId),
         params = req.body.text.split(":"),
         StageName = params[1],
-        name = params[0],
-        Opportunity conts=[select StageName from Opportunity where Name = name limit 1];
- 
-   conts.StageName= StageName;
-   update conts;
-
+        name = params[0];
         
-let q= "update StageName";
 
+force.update(oauthObj, "Opportunity",
+        {
+            Id:"062w000004ilulAAA",
+            StageName :StageName
+        })
 
-      force.query(oauthObj, q)
         .then(data => {
-            let opportunities = JSON.parse(data).records;
-            if (opportunities && opportunities.length > 0) {
-                let attachments = [];
-                opportunities.forEach(function (opportunity) {
-                    let fields = [];
+            
                     
-                    
-                    fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + opportunity.Id, short:false});
-                   
-                });
                 res.json({
                     text: "StageName Changed Successfully"
                  
                 });
-            } else {
-                res.send("No records");
-            }
+            
         })
         .catch(error => {
             if (error.code == 401) {
