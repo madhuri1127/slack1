8cff7a375c36b11a455c46ece83f8067faab9f12
madhuri1127 <68366270+madhuri1127@users.noreply.github.com>
2020-09-09 11:31:15 +0530

    Update opportunitystagechange.js

--- modules/opportunitystagechange.js
+++ modules/opportunitystagechange.js
@@ -15,9 +15,8 @@ exports.execute = (req, res) => {
         oauthObj = auth.getOAuthObject(slackUserId),
         params = req.body.text.split(":"),
         StageName = params[1],
-        name = params[0],
-q ="select id from Opportunity Where Name =name";
-        g = force.query(oauthObj,q);
+        name = params[0];
+
 
 force.update(oauthObj,"Opportunity",
         {
