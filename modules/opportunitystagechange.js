67bdb5a214a547a72723529ec0979992fff33eb7
madhuri1127 <68366270+madhuri1127@users.noreply.github.com>
2020-09-10 12:00:15 +0530

    Update opportunitystagechange.js

--- modules/opportunitystagechange.js
+++ modules/opportunitystagechange.js
@@ -17,7 +17,7 @@ exports.execute = (req, res) => {
         params = req.body.text.split(":"),
         StageName = params[1],
         name = params[0],
-        q="select id from Opportunity Where Name = name";
+        q="select id from Opportunity Limit 1";
     
   
     
